a68a99d91f22724aff680a206864292d9690701a
YASHMAROO <marooyash@yahoo.in>
2021-05-22 16:19:40 +0530

    Updated faculty page

--- src/components/Faculty.vue
+++ src/components/Faculty.vue
@@ -9,18 +9,22 @@
       <!-- Grid column -->
       <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
         <div class="avatar mx-auto">
-          <img src="@/assets/ECEHod.jpeg" class="rounded-circle z-depth-1" alt="Sample avatar" />
+          <img
+            src="@/assets/Durbadal Mandal.jpeg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
         </div>
-        <h5 class="font-weight-bold mt-4 mb-3">Prof. Asis Kumar Mal</h5>
+        <h5 class="font-weight-bold mt-4 mb-3">Durbadal Mandal</h5>
         <p class="text-uppercase blue-text">
           <strong>HOD</strong>
         </p>
         <p class="text-uppercase">
           <strong>Reseach Interest</strong>
         </p>
-        <p class="grey-text">Analog, and Mixed-Signal VLSI Digitally Assisted.</p>
+        <p class="grey-text">Antenna Optimization, Signal Processing, Soft Comp</p>
         <ul class="list-unstyled mb-0">
-          <a class="p-2 fa-lg ins-ic" href="mailto: akmal@ece.nitdgp.ac.in">
+          <a class="p-2 fa-lg ins-ic" href="mailto: durbadal.mondal@ece.nitdgp.ac.in">
             <i class="fas fa-envelope blue-text"></i>
           </a>
         </ul>
@@ -131,22 +135,18 @@
 
       <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
         <div class="avatar mx-auto">
-          <img
-            src="@/assets/Durbadal Mandal.jpeg"
-            class="rounded-circle z-depth-1"
-            alt="Sample avatar"
-          />
+          <img src="@/assets/ECEHod.jpeg" class="rounded-circle z-depth-1" alt="Sample avatar" />
         </div>
-        <h5 class="font-weight-bold mt-4 mb-3">Durbadal Mandal</h5>
+        <h5 class="font-weight-bold mt-4 mb-3">Prof. Asis Kumar Mal</h5>
         <p class="text-uppercase blue-text">
-          <strong>Assistant Professor</strong>
+          <strong>Professor</strong>
         </p>
         <p class="text-uppercase">
           <strong>Reseach Interest</strong>
         </p>
-        <p class="grey-text">Antenna Optimization, Signal Processing, Soft Comp</p>
+        <p class="grey-text">Analog, and Mixed-Signal VLSI Digitally Assisted.</p>
         <ul class="list-unstyled mb-0">
-          <a class="p-2 fa-lg ins-ic" href="mailto: durbadal.mondal@ece.nitdgp.ac.in">
+          <a class="p-2 fa-lg ins-ic" href="mailto: akmal@ece.nitdgp.ac.in">
             <i class="fas fa-envelope blue-text"></i>
           </a>
         </ul>
@@ -193,6 +193,256 @@
         <p class="grey-text">Neuromorphic computing, Parallel computing</p>
         <ul class="list-unstyled mb-0">
           <a class="p-2 fa-lg ins-ic" href="mailto: hemanta.mondal@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row pt-5">
+      <!-- Grid column -->
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/SDR Sir.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Dr. Sanjay Dhar Roy</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Associate Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Wireless Communication (Radio Resource Management, Cognitive Radio Networks, Heterogeneous Networks)</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: sanjay.dharroy@ece.nitdgp.ac.in ">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img src="@/assets/Nilanjan Sir.jpg" class="rounded-circle z-depth-1" alt="Sample avatar" />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Nilanjan Chattaraj</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Assistant Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Smart materials based devices and Structronic Systems</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: nilanjan.chattaraj@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/Rajat Sir.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Rajat Mahapatra</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Resistive Memory Devices,  HEMT Devices</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto:  rajat.mahapatra@ece.nitdgp.ac.in ">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/Rajib Kar.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Rajib Kar</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Associate Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">VLSI, Signal Processing</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: rajib.kar@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row pt-5">
+      <!-- Grid column -->
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/Rishi Todani.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Rishi Todani</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Project Faculty</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Analog and Mixed Signal Design</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: rishi.todani@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img src="@/assets/Rowdra Sir.jpg" class="rounded-circle z-depth-1" alt="Sample avatar" />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Rowdra Ghatak</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Fractal antenna, metamaterials</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: rowdra.ghatak@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/Sapna Mam.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Sapana Ranwa</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Assistant Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Metal Oxide Semiconductors based gas sensors</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto:  sapana.ranwa@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/Sujit SIr.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Sujit Kr. Mandal</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Assistant Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Synthesis of Antenna Arrays using Evolutionary Algorithms, Optimized Pattern Synthesis in Time-Modulated Antenna Arrays, etc.</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: sujit.mandal@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row pt-5">
+      <!-- Grid column -->
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/Sumit Kundu.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Sumit Kundu</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Professor (HAG)</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Wireless Communication & Networking</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: sumit.kundu@ece.nitdgp.ac.in">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img src="@/assets/Susanta Sir.jpg" class="rounded-circle z-depth-1" alt="Sample avatar" />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Susanta Sen</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Emeritus Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Quantum electron devices, VLSI Design</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto: susanta.rpe@gmail.com">
+            <i class="fas fa-envelope blue-text"></i>
+          </a>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
+        <div class="avatar mx-auto">
+          <img
+            src="@/assets/Tapas Sir.jpg"
+            class="rounded-circle z-depth-1"
+            alt="Sample avatar"
+          />
+        </div>
+        <h5 class="font-weight-bold mt-4 mb-3">Tapas Sadhukhan</h5>
+        <p class="text-uppercase blue-text">
+          <strong>Assistant Professor</strong>
+        </p>
+        <p class="text-uppercase">
+          <strong>Reseach Interest</strong>
+        </p>
+        <p class="grey-text">Signals, Systems, & Transmission</p>
+        <ul class="list-unstyled mb-0">
+          <a class="p-2 fa-lg ins-ic" href="mailto:  tapas.sadhukhan@ece.nitdgp.ac.in">
             <i class="fas fa-envelope blue-text"></i>
           </a>
         </ul>
